dfa40401f3c090779d963894fc47e20c6dc129d0
Kirstin Buchanan <buchananmkirstin@gmail.com>
2021-09-25 18:26:09 +0100

    work on mascot detail style

--- client/screens/MascotDetails.js
+++ client/screens/MascotDetails.js
@@ -8,47 +8,53 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
+import { Icon } from 'react-native-elements';
 
 const MascotDetails = ({ route }) => {
   const { mascot } = route.params;
 
   const handleOpenWithWebBrowser = () => {
+    console.log(mascot);
     WebBrowser.openBrowserAsync(mascot.officialSite);
   };
 
-  const makeFavourite = (mascot) => {
-    const favouriteMascot = { ...mascot, favourite: true };
-    return favouriteMascot;
-  };
+  // const makeFavourite = (mascot) => {
+  //   const favouriteMascot = { ...mascot, favourite: true };
+  //   return favouriteMascot;
+  // };
 
   return (
     <View style={styles.background}>
-      <Button title="Back" />
-      <TouchableOpacity onPress={() => makeFavourite}>
+      <Icon name="arrow-back-ios" type="materialicons" color="black" />
+      <View style={styles.top}>
+        {/* <TouchableOpacity onPress={() => makeFavourite}>
         {mascot.favourite ? <Text>Unlike</Text> : <Text>Like</Text>}
-      </TouchableOpacity>
-      <Text style={[styles.heading, styles.name]}>{mascot.name}</Text>
-      <Text style={[styles.japanese, styles.heading]}>{mascot.japanese}</Text>
-      <Image
-        style={styles.mascotImage}
-        source={{
-          uri: mascot.picture,
-        }}
-      />
+      </TouchableOpacity> */}
+        <Text style={[styles.heading, styles.name]}>{mascot.name}</Text>
+        <Text style={[styles.japanese, styles.heading]}>{mascot.japanese}</Text>
+        <Image
+          style={styles.mascotImage}
+          source={{
+            uri: mascot.picture,
+          }}
+        />
+      </View>
       <View style={styles.mascotAbout}>
-        <Text>Representing: {mascot.mascot}</Text>
-        <Text>City:{mascot.city}</Text>
-        <Text>Prefecture: {mascot.prefecture}</Text>
-        <Text>Description: {mascot.description}</Text>
-        {mascot.officialSite < 1 ? (
-          <Text></Text>
-        ) : (
-          <Button
-            title="Go to Offical Site"
-            onPress={() => handleOpenWithWebBrowser()}
-          />
-        )}
-        <Button title="Edit" />
+        <View style={styles.mascotText}>
+          <Text>Representing: {mascot.mascot}</Text>
+          <Text>City: {mascot.city}</Text>
+          <Text>Prefecture: {mascot.prefecture}</Text>
+          <Text>Description: {mascot.description}</Text>
+          {!mascot.officialSite ? (
+            <Text style={{ textAlign: 'center' }}>🌸</Text>
+          ) : (
+            <Button
+              title="Go to Offical Site"
+              onPress={() => handleOpenWithWebBrowser()}
+            />
+          )}
+          {/* <Button title="Edit" /> */}
+        </View>
       </View>
     </View>
   );
@@ -56,7 +62,7 @@ const MascotDetails = ({ route }) => {
 
 const styles = StyleSheet.create({
   heading: {
-    marginLeft: 10,
+    marginLeft: 20,
   },
   name: {
     fontSize: 24,
@@ -69,17 +75,28 @@ const styles = StyleSheet.create({
   background: {
     backgroundColor: 'pink',
     zIndex: -1,
+    height: '100%',
   },
   mascotImage: {
     height: 300,
     width: 400,
     resizeMode: 'center',
-    zIndex: 3,
+    justifyContent: 'center',
+    zIndex: 10,
     borderRadius: 30,
+    borderColor: 'red',
+    borderWidth: 1,
   },
+  top: {
+    flex: 1,
+  },
+  mascotText: { margin: 30 },
   mascotAbout: {
     backgroundColor: 'white',
-    zIndex: 2,
+    zIndex: 1,
+    flex: 1,
+    borderTopStartRadius: 30,
+    borderTopEndRadius: 30,
   },
 });
 
