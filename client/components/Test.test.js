f345bc3372b607256830eeefb22645669d96c961
Aline Silva <lisie_su@yahoo.com.br>
2021-10-01 15:41:19 +0100

    Testing

---
+++ client/components/Test.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {cleanup, render} from '@testing-library/react-native';
+
+import Test from './Test';
+
+afterEach(cleanup);
+
+describe('Test', () => {
+  it('should show hello world', () => {
+    const helloWorldText = 'Hello World!';
+
+    const {toJSON, getByText} = render(<Test />);
+
+    const foundHelloWorldText = getByText(helloWorldText);
+
+    expect(foundHelloWorldText.props.children).toEqual(helloWorldText);
+    expect(toJSON()).toMatchSnapshot();
+  });
+});
---
+++ client/screens/Home.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+
+import MascotList from "../components/MascotList";
+
+
+it("renders", () => {
+    render(<MascotList />);
+});
